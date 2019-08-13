c5a71466263de5ec2d40496617a85f44b74954f1
redfast00 <redfast00@gmail.com>
2019-08-13 17:56:24 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rcpu_compiler
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject rcpu_compiler "0.1.0-SNAPSHOT"
+  :description "A compiler for RCPU"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot rcpu-compiler.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
